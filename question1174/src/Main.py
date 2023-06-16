fa9d1aa61c8267c8ccc9096a714f349c15ad519f
furkansenkal <72168386+furkansenkal@users.noreply.github.com>
2023-06-17 00:53:12 +0300

    Add files via upload

---
+++ question1174/src/Main.py
@@ -0,0 +1,36 @@
+
+def input_to_list(_str):
+    str_lst = _str.split()
+    if(len(str_lst) > 0):
+        return [int(elem) for elem in str_lst]
+    else:
+        return []
+
+
+def find_difference(lst1, lst2):
+    # DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE
+
+    difflst=[]
+
+    for i in lst1:
+
+        if i in lst2:
+            continue
+
+        else:
+            if i in difflst:
+                continue
+            difflst.append(i)
+
+    print(difflst)
+
+    # DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE
+    return
+
+
+input_str1 = input()
+input_str2 = input()
+input_lst1 = input_to_list(input_str1)
+input_lst2 = input_to_list(input_str2)
+find_difference(input_lst1, input_lst2)
+
